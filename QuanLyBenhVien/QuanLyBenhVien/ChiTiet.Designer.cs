49efdc682a470a55cb331dfb18649b9355bd11d1
tanhao3009 <hao.phamtan@gmail.com>
2016-12-14 20:57:31 +0700

    Add files via upload
    Đã có chức năng thêm thông tin, giao diện bệnh viện

---
+++ QuanLyBenhVien/QuanLyBenhVien/ChiTiet.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace QuanLyBenhVien
+{
+    partial class ChiTiet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridViewChiTiet = new System.Windows.Forms.DataGridView();
+            this.txtMaYTe = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.txtNamSinh = new System.Windows.Forms.TextBox();
+            this.txtGioiTinh = new System.Windows.Forms.TextBox();
+            this.txtNgheNghiep = new System.Windows.Forms.TextBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.txtSoDT = new System.Windows.Forms.TextBox();
+            this.txtChuanDoan = new System.Windows.Forms.TextBox();
+            this.txtTenThuoc = new System.Windows.Forms.TextBox();
+            this.txtLoiDan = new System.Windows.Forms.TextBox();
+            this.txtNgayTaiKham = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.abtnReLoad = new System.Windows.Forms.Button();
+            this.abtnThem = new System.Windows.Forms.Button();
+            this.abtnSua = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewChiTiet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridViewChiTiet
+            // 
+            this.gridViewChiTiet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewChiTiet.Location = new System.Drawing.Point(1, 34);
+            this.gridViewChiTiet.Name = "gridViewChiTiet";
+            this.gridViewChiTiet.Size = new System.Drawing.Size(670, 214);
+            this.gridViewChiTiet.TabIndex = 0;
+            this.gridViewChiTiet.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridViewChiTiet_RowEnter);
+            // 
+            // txtMaYTe
+            // 
+            this.txtMaYTe.Location = new System.Drawing.Point(97, 254);
+            this.txtMaYTe.Name = "txtMaYTe";
+            this.txtMaYTe.Size = new System.Drawing.Size(120, 20);
+            this.txtMaYTe.TabIndex = 1;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(97, 289);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(120, 20);
+            this.txtHoTen.TabIndex = 2;
+            // 
+            // txtNamSinh
+            // 
+            this.txtNamSinh.Location = new System.Drawing.Point(97, 326);
+            this.txtNamSinh.Name = "txtNamSinh";
+            this.txtNamSinh.Size = new System.Drawing.Size(120, 20);
+            this.txtNamSinh.TabIndex = 3;
+            // 
+            // txtGioiTinh
+            // 
+            this.txtGioiTinh.Location = new System.Drawing.Point(97, 363);
+            this.txtGioiTinh.Name = "txtGioiTinh";
+            this.txtGioiTinh.Size = new System.Drawing.Size(120, 20);
+            this.txtGioiTinh.TabIndex = 4;
+            // 
+            // txtNgheNghiep
+            // 
+            this.txtNgheNghiep.Location = new System.Drawing.Point(97, 401);
+            this.txtNgheNghiep.Name = "txtNgheNghiep";
+            this.txtNgheNghiep.Size = new System.Drawing.Size(120, 20);
+            this.txtNgheNghiep.TabIndex = 5;
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(97, 439);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(120, 20);
+            this.txtDiaChi.TabIndex = 6;
+            // 
+            // txtSoDT
+            // 
+            this.txtSoDT.Location = new System.Drawing.Point(381, 254);
+            this.txtSoDT.Name = "txtSoDT";
+            this.txtSoDT.Size = new System.Drawing.Size(135, 20);
+            this.txtSoDT.TabIndex = 7;
+            // 
+            // txtChuanDoan
+            // 
+            this.txtChuanDoan.Location = new System.Drawing.Point(381, 289);
+            this.txtChuanDoan.Name = "txtChuanDoan";
+            this.txtChuanDoan.Size = new System.Drawing.Size(135, 20);
+            this.txtChuanDoan.TabIndex = 8;
+            // 
+            // txtTenThuoc
+            // 
+            this.txtTenThuoc.Location = new System.Drawing.Point(381, 326);
+            this.txtTenThuoc.Name = "txtTenThuoc";
+            this.txtTenThuoc.Size = new System.Drawing.Size(135, 20);
+            this.txtTenThuoc.TabIndex = 9;
+            // 
+            // txtLoiDan
+            // 
+            this.txtLoiDan.Location = new System.Drawing.Point(381, 363);
+            this.txtLoiDan.Name = "txtLoiDan";
+            this.txtLoiDan.Size = new System.Drawing.Size(135, 20);
+            this.txtLoiDan.TabIndex = 10;
+            // 
+            // txtNgayTaiKham
+            // 
+            this.txtNgayTaiKham.Location = new System.Drawing.Point(381, 401);
+            this.txtNgayTaiKham.Name = "txtNgayTaiKham";
+            this.txtNgayTaiKham.Size = new System.Drawing.Size(135, 20);
+            this.txtNgayTaiKham.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Mã Y Tế";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Họ và Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 329);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Năm Sinh";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Giới Tính";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 404);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Nghề Nghiệp";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 442);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Địa Chỉ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(282, 260);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Số Điện Thoại";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(282, 292);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Chuẩn Đoán";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(282, 329);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Tên Thuốc";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(282, 366);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Lời Dặn";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(282, 404);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(80, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Ngày Tái Khám";
+            // 
+            // abtnReLoad
+            // 
+            this.abtnReLoad.Location = new System.Drawing.Point(596, 5);
+            this.abtnReLoad.Name = "abtnReLoad";
+            this.abtnReLoad.Size = new System.Drawing.Size(75, 23);
+            this.abtnReLoad.TabIndex = 25;
+            this.abtnReLoad.Text = "Tải Lại";
+            this.abtnReLoad.UseVisualStyleBackColor = true;
+            this.abtnReLoad.Click += new System.EventHandler(this.abtnReLoad_Click);
+            // 
+            // abtnThem
+            // 
+            this.abtnThem.Location = new System.Drawing.Point(555, 260);
+            this.abtnThem.Name = "abtnThem";
+            this.abtnThem.Size = new System.Drawing.Size(116, 34);
+            this.abtnThem.TabIndex = 27;
+            this.abtnThem.Text = "Thêm";
+            this.abtnThem.UseVisualStyleBackColor = true;
+            this.abtnThem.Click += new System.EventHandler(this.abtnThem_Click);
+            // 
+            // abtnSua
+            // 
+            this.abtnSua.Location = new System.Drawing.Point(555, 318);
+            this.abtnSua.Name = "abtnSua";
+            this.abtnSua.Size = new System.Drawing.Size(116, 30);
+            this.abtnSua.TabIndex = 28;
+            this.abtnSua.Text = "Sửa";
+            this.abtnSua.UseVisualStyleBackColor = true;
+            this.abtnSua.Click += new System.EventHandler(this.abtnSua_Click);
+            // 
+            // ChiTiet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(682, 475);
+            this.Controls.Add(this.abtnSua);
+            this.Controls.Add(this.abtnThem);
+            this.Controls.Add(this.abtnReLoad);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNgayTaiKham);
+            this.Controls.Add(this.txtLoiDan);
+            this.Controls.Add(this.txtTenThuoc);
+            this.Controls.Add(this.txtChuanDoan);
+            this.Controls.Add(this.txtSoDT);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.txtNgheNghiep);
+            this.Controls.Add(this.txtGioiTinh);
+            this.Controls.Add(this.txtNamSinh);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.txtMaYTe);
+            this.Controls.Add(this.gridViewChiTiet);
+            this.Name = "ChiTiet";
+            this.Text = "QuanLyBenhVien-ChiTiet";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ChiTiet_FormClosing);
+            this.Load += new System.EventHandler(this.ChiTiet_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewChiTiet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridViewChiTiet;
+        private System.Windows.Forms.TextBox txtMaYTe;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.TextBox txtNamSinh;
+        private System.Windows.Forms.TextBox txtGioiTinh;
+        private System.Windows.Forms.TextBox txtNgheNghiep;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.TextBox txtSoDT;
+        private System.Windows.Forms.TextBox txtChuanDoan;
+        private System.Windows.Forms.TextBox txtTenThuoc;
+        private System.Windows.Forms.TextBox txtLoiDan;
+        private System.Windows.Forms.TextBox txtNgayTaiKham;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button abtnReLoad;
+        private System.Windows.Forms.Button abtnThem;
+        private System.Windows.Forms.Button abtnSua;
+    }
+}
+
---
+++ QuanLyBenhVien/QuanLyBenhVien/DAL/BenhVienDAO.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using QuanLyBenhVien.EF;
+
+
+namespace QuanLyBenhVien.DAL
+{
+    class BenhVienDAO
+    {
+        public DataTable LoadAll()
+        {
+            String connect = ConfigurationManager.ConnectionStrings["data"].ConnectionString;
+            SqlConnection con = new SqlConnection(connect);
+            con.Open();
+            DataTable dt = new DataTable();
+            String query = "select b.MaYTe,b.HoTen,b.NamSinh,b.GioiTinh,b.NgheNghiep,b.DiaChi,b.SoDT, l.ChuanDoan,l.TenThuoc,l.LoiDan,l.NgayTaiKham from BenhNhan b, LichSuKhamBenh l where b.MaYTe = l.MaYTe";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(dt);
+            con.Close();
+            return dt;
+        }
+        public bool Insert(BenhVienDTO bv)
+        {
+            bool result = false;
+            String connect = ConfigurationManager.ConnectionStrings["data"].ConnectionString;
+            SqlConnection con = new SqlConnection(connect);
+            con.Open();
+            String query = "INSERT INTO BenhNhan(MaYTe,HoTen,NamSinh,GioiTinh,DiaChi,NgheNghiep,SoDT) VALUES (@MaYTe,@HoTen,@NamSinh,@GioiTinh,@DiaChi,@NgheNghiep,@SoDT); INSERT INTO LichSuKhamBenh(MaYTe,ChuanDoan,TenThuoc,LoiDan,NgayTaiKham) VALUES (@MaYTe,@ChuanDoan,@TenThuoc,@LoiDan,@NgayTaiKham);";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.Add(new SqlParameter("@MaYTe", bv.MaYTe));
+            cmd.Parameters.Add(new SqlParameter("@HoTen", bv.HoTen));
+            cmd.Parameters.Add(new SqlParameter("@NamSinh", bv.NamSinh));
+            cmd.Parameters.Add(new SqlParameter("@GioiTinh", bv.GioiTinh));
+            cmd.Parameters.Add(new SqlParameter("@DiaChi", bv.DiaChi));
+            cmd.Parameters.Add(new SqlParameter("@NgheNghiep", bv.NgheNghiep));
+            cmd.Parameters.Add(new SqlParameter("@SoDT", bv.SoDT));
+            cmd.Parameters.Add(new SqlParameter("@ChuanDoan", bv.ChuanDoan));
+            cmd.Parameters.Add(new SqlParameter("@TenThuoc", bv.TenThuoc));
+            cmd.Parameters.Add(new SqlParameter("@LoiDan", bv.LoiDan));
+            cmd.Parameters.Add(new SqlParameter("@NgayTaiKham", bv.NgayTaiKham));
+            int rows = cmd.ExecuteNonQuery();
+            if (rows > 0) result = true;
+            con.Close();
+            return result;
+        }
+        
+        
+
+        
+    }
+}
