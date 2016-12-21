061b16fe1b3eeb30c6ce431f2c0c590699248333
Dao Nguyen <LightWay152@users.noreply.github.com>
2016-12-21 21:30:06 +0700

    Add files via upload
    Completed Project - Fix bugs of version 3.0 which using DevExpress

---
+++ QuanLyBenhVien/QuanLyBenhVien/Model/DAO/BenhNhanDAO.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QTCC_QuanLyBenhVien.Model.DTO;
+
+namespace QTCC_QuanLyBenhVien.Model.DAO
+{
+    public class BenhNhanDAO
+    {
+        private readonly QLBenhVienDBContext _db;
+
+        public BenhNhanDAO()
+        {
+            _db = new QLBenhVienDBContext();
+        }
+
+        public string AutoIncreaseMaYte()
+        {
+            string Id = "MYT";
+            long subId = 2130000;
+            long count = _db.BenhNhans.Count(x => x.MaYTe.Contains(Id));
+            count = count + subId;
+            BenhNhan benhnhan = new BenhNhan();
+            benhnhan = _db.BenhNhans.SingleOrDefault(x => x.MaYTe.Equals(Id + count));
+            while (benhnhan != null)
+            {
+                count += 1;
+                benhnhan = _db.BenhNhans.SingleOrDefault(x => x.MaYTe.Equals(Id + count));
+            }
+            return Id + count;
+        }
+
+        public List<BenhNhan> GetAllBenhnhan()
+        {
+            return _db.BenhNhans.OrderByDescending(x => x.MaYTe).ToList();
+        }
+
+        public BenhNhan GetBenhNhanById(string id)
+        {
+            return _db.BenhNhans.SingleOrDefault(x => x.MaYTe==id);
+        }
+
+        public bool ThemBenhNhan(BenhNhan benhnhan)
+        {
+            if (GetBenhNhanById(benhnhan.MaYTe) == null)
+            {
+                try
+                {
+                    _db.BenhNhans.Add(benhnhan);
+                    _db.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                    throw;
+                }
+            }
+            return false;
+        }
+
+        public bool UpdateBenhnhan(BenhNhan benhnhan)
+        {
+            var bn = GetBenhNhanById(benhnhan.MaYTe);
+            if ( bn!= null)
+            {
+                try
+                {
+                    bn.MaYTe = benhnhan.MaYTe;
+                    bn.DiaChi = benhnhan.DiaChi;
+                    bn.GioiTinh = benhnhan.GioiTinh;
+                    bn.HoTen = benhnhan.HoTen;
+                    bn.NamSinh = benhnhan.NamSinh;
+                    bn.NgheNghiep = benhnhan.NgheNghiep;
+                    bn.SoDT = benhnhan.SoDT;
+                    _db.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                    throw;
+                }
+            }
+            return false;
+        }
+
+        public bool DeleteBenhNhan(string id)
+        {
+            var bn = GetBenhNhanById(id);
+            if (bn != null)
+            {
+                if (_db.LichSuKhamBenhs.Count(x => x.MaYTe==id) > 0)
+                {
+                    try
+                    {
+                        _db.BenhNhans.Remove(bn);
+                        List<LichSuKhamBenh> ls = _db.LichSuKhamBenhs.Where(x => x.MaYTe == id).ToList();
+                        _db.LichSuKhamBenhs.RemoveRange(ls);
+                        _db.SaveChanges();
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        _db.BenhNhans.Remove(bn);
+                        _db.SaveChanges();
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ QuanLyBenhVien/QuanLyBenhVien/Model/DTO - Copy/BenhNhan.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace QTCC_QuanLyBenhVien.Model.DTO
+{
+    [Table("BenhNhan")]
+    public class BenhNhan
+    {
+        [SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public BenhNhan()
+        {
+            // ReSharper disable once DoNotCallOverridableMethodsInConstructor
+            LichSuKhamBenhs = new HashSet<LichSuKhamBenh>();
+        }
+
+        [Key]
+        [StringLength(50)]
+        [Display(Name = @"Mã Y tế", Description = @"Mã Y tế")]
+        public string MaYTe { get; set; }
+
+        [Required]
+        [Display(Name = @"Họ tên", Description = @"Họ tên")]
+        public string HoTen { get; set; }
+
+        [Required]
+        [Display(Name = @"Năm sinh", Description = @"Năm sinh")]
+        [StringLength(50)]
+        public string NamSinh { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = @"Giới tính", Description = @"Giới tính")]
+        public string GioiTinh { get; set; }
+
+        [Required]
+        [Display(Name = @"Địa chỉ", Description = @"Địa chỉ")]
+        public string DiaChi { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = @"Nghề nghiệp", Description = @"Nghề nghiệp")]
+        public string NgheNghiep { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = @"Số điện thoại", Description = @"Số điện thoại")]
+        public string SoDT { get; set; }
+
+        [SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<LichSuKhamBenh> LichSuKhamBenhs { get; set; }
+    }
+}
