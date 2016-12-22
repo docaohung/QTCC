d6631a3759cab07d1189be6cdbb5667776ef4a18
Dao Nguyen <LightWay152@users.noreply.github.com>
2016-12-22 20:34:25 +0700

    Add files via upload
    Fix bugs about Login, logic, hint for field and complete project.

---
+++ QuanLyBenhVien-master/DangNhap.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+using QTCC_QuanLyBenhVien.Model.DAO;
+
+namespace QTCC_QuanLyBenhVien
+{
+    public partial class LoginForm : DevExpress.XtraEditors.XtraForm
+    {
+        private readonly UserDAO _userDao;
+        public LoginForm()
+        {
+            InitializeComponent();
+            _userDao = new UserDAO();
+            txtPwd.Properties.UseSystemPasswordChar = true;
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsername_EditValueChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUsername.Text))
+            {
+                dxErrorProviderDangnhap.SetError(txtUsername, "Vui lòng nhập username");
+            }
+        }
+
+        private void txtPwd_EditValueChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPwd.Text))
+            {
+                dxErrorProviderDangnhap.SetError(txtPwd, "Vui lòng nhập password");
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var result = _userDao.Login(txtUsername.Text, txtPwd.Text);
+            if (result == 1)
+            {
+                var formql = new DanhSachBenhNhan();
+                formql.Show();
+                if (!cbRemember.Checked)
+                {
+                    txtUsername.Text = null;
+                    txtPwd.Text = null;
+                }
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại");
+            }
+        }
+    }
+}
---
+++ QuanLyBenhVien-master/ThemCapNhapBenhNhan.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace QTCC_QuanLyBenhVien
+{
+    partial class ThemCapNhapBenhNhan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ThemCapNhapBenhNhan));
+            this.ribbon = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.btnSave = new DevExpress.XtraBars.BarButtonItem();
+            this.btnExit = new DevExpress.XtraBars.BarButtonItem();
+            this.btnThemHoSo = new DevExpress.XtraBars.BarButtonItem();
+            this.btnUpdateHoSo = new DevExpress.XtraBars.BarButtonItem();
+            this.btnXoaHoso = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.txtGioiTinh = new System.Windows.Forms.ComboBox();
+            this.txtNgaySinh = new DevExpress.XtraEditors.DateEdit();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSoDienthoai = new System.Windows.Forms.TextBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.txtNgheNghiep = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.txtMayte = new System.Windows.Forms.TextBox();
+            this.dxErrorProvider1 = new DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNgaySinh.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNgaySinh.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dxErrorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbon
+            // 
+            this.ribbon.ExpandCollapseItem.Id = 0;
+            this.ribbon.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbon.ExpandCollapseItem,
+            this.btnSave,
+            this.btnExit,
+            this.btnThemHoSo,
+            this.btnUpdateHoSo,
+            this.btnXoaHoso});
+            this.ribbon.Location = new System.Drawing.Point(0, 0);
+            this.ribbon.MaxItemId = 9;
+            this.ribbon.Name = "ribbon";
+            this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            this.ribbon.Size = new System.Drawing.Size(460, 143);
+            this.ribbon.StatusBar = this.ribbonStatusBar;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Caption = "Lưu";
+            this.btnSave.Id = 2;
+            this.btnSave.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.ImageOptions.Image")));
+            this.btnSave.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnSave.ImageOptions.LargeImage")));
+            this.btnSave.Name = "btnSave";
+            this.btnSave.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnSave_ItemClick);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Caption = "Exit";
+            this.btnExit.Id = 3;
+            this.btnExit.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.ImageOptions.Image")));
+            this.btnExit.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnExit.ImageOptions.LargeImage")));
+            this.btnExit.Name = "btnExit";
+            this.btnExit.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnExit_ItemClick);
+            // 
+            // btnThemHoSo
+            // 
+            this.btnThemHoSo.Caption = "Thêm hồ sơ khám bệnh";
+            this.btnThemHoSo.Id = 6;
+            this.btnThemHoSo.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnThemHoSo.ImageOptions.Image")));
+            this.btnThemHoSo.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnThemHoSo.ImageOptions.LargeImage")));
+            this.btnThemHoSo.Name = "btnThemHoSo";
+            this.btnThemHoSo.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnThemHoSo_ItemClick);
+            // 
+            // btnUpdateHoSo
+            // 
+            this.btnUpdateHoSo.Caption = "Cập nhật hồ sơ khám bệnh";
+            this.btnUpdateHoSo.Id = 7;
+            this.btnUpdateHoSo.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnUpdateHoSo.ImageOptions.Image")));
+            this.btnUpdateHoSo.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnUpdateHoSo.ImageOptions.LargeImage")));
+            this.btnUpdateHoSo.Name = "btnUpdateHoSo";
+            // 
+            // btnXoaHoso
+            // 
+            this.btnXoaHoso.Caption = "Xóa hồ sơ khám bệnh";
+            this.btnXoaHoso.Id = 8;
+            this.btnXoaHoso.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnXoaHoso.ImageOptions.Image")));
+            this.btnXoaHoso.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnXoaHoso.ImageOptions.LargeImage")));
+            this.btnXoaHoso.Name = "btnXoaHoso";
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "Tác vụ";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnSave);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnExit);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "Quản lý nội dung";
+            // 
+            // ribbonStatusBar
+            // 
+            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 418);
+            this.ribbonStatusBar.Name = "ribbonStatusBar";
+            this.ribbonStatusBar.Ribbon = this.ribbon;
+            this.ribbonStatusBar.Size = new System.Drawing.Size(460, 31);
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.txtGioiTinh);
+            this.groupControl1.Controls.Add(this.txtNgaySinh);
+            this.groupControl1.Controls.Add(this.label4);
+            this.groupControl1.Controls.Add(this.label3);
+            this.groupControl1.Controls.Add(this.label7);
+            this.groupControl1.Controls.Add(this.label6);
+            this.groupControl1.Controls.Add(this.label5);
+            this.groupControl1.Controls.Add(this.label2);
+            this.groupControl1.Controls.Add(this.label1);
+            this.groupControl1.Controls.Add(this.txtSoDienthoai);
+            this.groupControl1.Controls.Add(this.txtDiaChi);
+            this.groupControl1.Controls.Add(this.txtNgheNghiep);
+            this.groupControl1.Controls.Add(this.txtHoTen);
+            this.groupControl1.Controls.Add(this.txtMayte);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControl1.Location = new System.Drawing.Point(0, 143);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(460, 275);
+            this.groupControl1.TabIndex = 2;
+            this.groupControl1.Text = "Thông tin bệnh nhân";
+            // 
+            // txtGioiTinh
+            // 
+            this.txtGioiTinh.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.txtGioiTinh.FormattingEnabled = true;
+            this.txtGioiTinh.Location = new System.Drawing.Point(354, 66);
+            this.txtGioiTinh.Name = "txtGioiTinh";
+            this.txtGioiTinh.Size = new System.Drawing.Size(55, 21);
+            this.txtGioiTinh.TabIndex = 3;
+            // 
+            // txtNgaySinh
+            // 
+            this.txtNgaySinh.EditValue = null;
+            this.txtNgaySinh.Location = new System.Drawing.Point(82, 94);
+            this.txtNgaySinh.MenuManager = this.ribbon;
+            this.txtNgaySinh.Name = "txtNgaySinh";
+            this.txtNgaySinh.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.txtNgaySinh.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.txtNgaySinh.Size = new System.Drawing.Size(127, 20);
+            this.txtNgaySinh.TabIndex = 2;
+            this.txtNgaySinh.EditValueChanged += new System.EventHandler(this.txtNgaySinh_EditValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(289, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Giới tính:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Ngày Sinh:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 210);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Số điện thoại:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Nghề nghiệp:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Địa chỉ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Họ và tên:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã y tế:";
+            // 
+            // txtSoDienthoai
+            // 
+            this.txtSoDienthoai.Location = new System.Drawing.Point(82, 207);
+            this.txtSoDienthoai.Name = "txtSoDienthoai";
+            this.txtSoDienthoai.Size = new System.Drawing.Size(184, 21);
+            this.txtSoDienthoai.TabIndex = 0;
+            this.txtSoDienthoai.TextChanged += new System.EventHandler(this.txtSoDienthoai_TextChanged);
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(82, 120);
+            this.txtDiaChi.Multiline = true;
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(327, 45);
+            this.txtDiaChi.TabIndex = 0;
+            this.txtDiaChi.TextChanged += new System.EventHandler(this.txtDiaChi_TextChanged);
+            // 
+            // txtNgheNghiep
+            // 
+            this.txtNgheNghiep.Location = new System.Drawing.Point(82, 180);
+            this.txtNgheNghiep.Name = "txtNgheNghiep";
+            this.txtNgheNghiep.Size = new System.Drawing.Size(184, 21);
+            this.txtNgheNghiep.TabIndex = 0;
+            this.txtNgheNghiep.TextChanged += new System.EventHandler(this.txtNgheNghiep_TextChanged);
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(82, 66);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(184, 21);
+            this.txtHoTen.TabIndex = 0;
+            this.txtHoTen.TextChanged += new System.EventHandler(this.txtHoTen_TextChanged);
+            // 
+            // txtMayte
+            // 
+            this.txtMayte.Location = new System.Drawing.Point(82, 39);
+            this.txtMayte.Name = "txtMayte";
+            this.txtMayte.ReadOnly = true;
+            this.txtMayte.Size = new System.Drawing.Size(127, 21);
+            this.txtMayte.TabIndex = 0;
+            // 
+            // dxErrorProvider1
+            // 
+            this.dxErrorProvider1.ContainerControl = this;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ThemCapNhapBenhNhan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 449);
+            this.Controls.Add(this.groupControl1);
+            this.Controls.Add(this.ribbonStatusBar);
+            this.Controls.Add(this.ribbon);
+            this.Name = "ThemCapNhapBenhNhan";
+            this.Ribbon = this.ribbon;
+            this.StatusBar = this.ribbonStatusBar;
+            this.Text = "Thêm/Cập nhật bệnh nhân";
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNgaySinh.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNgaySinh.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dxErrorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbon;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraBars.BarButtonItem btnSave;
+        private DevExpress.XtraBars.BarButtonItem btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.TextBox txtMayte;
+        private System.Windows.Forms.Label label2;
+        private DevExpress.XtraEditors.DateEdit txtNgaySinh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox txtGioiTinh;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNgheNghiep;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSoDienthoai;
+        private DevExpress.XtraBars.BarButtonItem btnThemHoSo;
+        private DevExpress.XtraBars.BarButtonItem btnUpdateHoSo;
+        private DevExpress.XtraBars.BarButtonItem btnXoaHoso;
+        private DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider dxErrorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
