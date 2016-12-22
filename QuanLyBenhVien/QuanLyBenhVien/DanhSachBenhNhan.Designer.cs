689c7cb0f043394069a964dbe88cea8f3e26d33c
trucdt1995 <trucdt1995@gmail.com>
2016-12-23 01:25:45 +0700

    Add files via upload
    Using DevExpress to customize interface of program

---
+++ QuanLyBenhVien/QuanLyBenhVien/DanhSachBenhNhan.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace QTCC_QuanLyBenhVien
+{
+    partial class DanhSachBenhNhan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DanhSachBenhNhan));
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.benhNhansBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colMaYTe = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colHoTen = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colNamSinh = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colGioiTinh = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDiaChi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colNgheNghiep = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSoDT = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colLichSuKhamBenhs = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.btnThem = new DevExpress.XtraBars.BarButtonItem();
+            this.btnCapnhat = new DevExpress.XtraBars.BarButtonItem();
+            this.btnXoa = new DevExpress.XtraBars.BarButtonItem();
+            this.btnDanhSachHoSo = new DevExpress.XtraBars.BarButtonItem();
+            this.btnExit = new DevExpress.XtraBars.BarButtonItem();
+            this.btnRefresh = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPageGroup3 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.cardView1 = new DevExpress.XtraGrid.Views.Card.CardView();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.benhNhansBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.benhNhansBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(2, 20);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.MenuManager = this.ribbonControl1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(754, 195);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1,
+            this.cardView1});
+            this.gridControl1.Click += new System.EventHandler(this.gridControl1_Click);
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colMaYTe,
+            this.colHoTen,
+            this.colNamSinh,
+            this.colGioiTinh,
+            this.colDiaChi,
+            this.colNgheNghiep,
+            this.colSoDT,
+            this.colLichSuKhamBenhs});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsCustomization.AllowColumnMoving = false;
+            this.gridView1.OptionsCustomization.CustomizationFormSearchBoxVisible = true;
+            this.gridView1.OptionsFind.AlwaysVisible = true;
+            this.gridView1.OptionsFind.FindDelay = 200;
+            this.gridView1.OptionsFind.FindNullPrompt = "Nhập vào thông tin bệnh nhân(mã y tế, họ tên...) để tìm kiếm";
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            this.gridView1.FocusedRowChanged += new DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventHandler(this.gridView1_FocusedRowChanged);
+            this.gridView1.RowUpdated += new DevExpress.XtraGrid.Views.Base.RowObjectEventHandler(this.gridView1_RowUpdated);
+            this.gridView1.RowCountChanged += new System.EventHandler(this.gridView1_RowCountChanged);
+            // 
+            // colMaYTe
+            // 
+            this.colMaYTe.FieldName = "MaYTe";
+            this.colMaYTe.Name = "colMaYTe";
+            this.colMaYTe.OptionsColumn.ReadOnly = true;
+            this.colMaYTe.Visible = true;
+            this.colMaYTe.VisibleIndex = 0;
+            // 
+            // colHoTen
+            // 
+            this.colHoTen.FieldName = "HoTen";
+            this.colHoTen.Name = "colHoTen";
+            this.colHoTen.Visible = true;
+            this.colHoTen.VisibleIndex = 1;
+            // 
+            // colNamSinh
+            // 
+            this.colNamSinh.FieldName = "NamSinh";
+            this.colNamSinh.Name = "colNamSinh";
+            this.colNamSinh.Visible = true;
+            this.colNamSinh.VisibleIndex = 2;
+            // 
+            // colGioiTinh
+            // 
+            this.colGioiTinh.FieldName = "GioiTinh";
+            this.colGioiTinh.Name = "colGioiTinh";
+            this.colGioiTinh.OptionsColumn.ReadOnly = true;
+            this.colGioiTinh.Visible = true;
+            this.colGioiTinh.VisibleIndex = 3;
+            // 
+            // colDiaChi
+            // 
+            this.colDiaChi.FieldName = "DiaChi";
+            this.colDiaChi.Name = "colDiaChi";
+            this.colDiaChi.Visible = true;
+            this.colDiaChi.VisibleIndex = 4;
+            // 
+            // colNgheNghiep
+            // 
+            this.colNgheNghiep.FieldName = "NgheNghiep";
+            this.colNgheNghiep.Name = "colNgheNghiep";
+            this.colNgheNghiep.Visible = true;
+            this.colNgheNghiep.VisibleIndex = 5;
+            // 
+            // colSoDT
+            // 
+            this.colSoDT.FieldName = "SoDT";
+            this.colSoDT.Name = "colSoDT";
+            this.colSoDT.Visible = true;
+            this.colSoDT.VisibleIndex = 6;
+            // 
+            // colLichSuKhamBenhs
+            // 
+            this.colLichSuKhamBenhs.FieldName = "LichSuKhamBenhs";
+            this.colLichSuKhamBenhs.Name = "colLichSuKhamBenhs";
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.ExpandCollapseItem.Id = 0;
+            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem,
+            this.btnThem,
+            this.btnCapnhat,
+            this.btnXoa,
+            this.btnDanhSachHoSo,
+            this.btnExit,
+            this.btnRefresh});
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.MaxItemId = 10;
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            this.ribbonControl1.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2010;
+            this.ribbonControl1.Size = new System.Drawing.Size(758, 143);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Caption = "Thêm";
+            this.btnThem.Id = 1;
+            this.btnThem.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnThem.ImageOptions.Image")));
+            this.btnThem.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnThem.ImageOptions.LargeImage")));
+            this.btnThem.Name = "btnThem";
+            this.btnThem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnThem_ItemClick);
+            // 
+            // btnCapnhat
+            // 
+            this.btnCapnhat.Caption = "Cập nhật";
+            this.btnCapnhat.Id = 2;
+            this.btnCapnhat.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnCapnhat.ImageOptions.Image")));
+            this.btnCapnhat.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnCapnhat.ImageOptions.LargeImage")));
+            this.btnCapnhat.Name = "btnCapnhat";
+            this.btnCapnhat.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnCapnhat_ItemClick);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Caption = "Xóa";
+            this.btnXoa.Id = 3;
+            this.btnXoa.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnXoa.ImageOptions.Image")));
+            this.btnXoa.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnXoa.ImageOptions.LargeImage")));
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnXoa_ItemClick);
+            // 
+            // btnDanhSachHoSo
+            // 
+            this.btnDanhSachHoSo.Caption = "Danh sách hồ sơ";
+            this.btnDanhSachHoSo.Id = 7;
+            this.btnDanhSachHoSo.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnDanhSachHoSo.ImageOptions.Image")));
+            this.btnDanhSachHoSo.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnDanhSachHoSo.ImageOptions.LargeImage")));
+            this.btnDanhSachHoSo.Name = "btnDanhSachHoSo";
+            this.btnDanhSachHoSo.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnDanhSachHoSo_ItemClick);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Caption = "Thoát";
+            this.btnExit.Id = 8;
+            this.btnExit.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.ImageOptions.Image")));
+            this.btnExit.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnExit.ImageOptions.LargeImage")));
+            this.btnExit.Name = "btnExit";
+            this.btnExit.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnExit_ItemClick);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Caption = "Tải lại";
+            this.btnRefresh.Id = 9;
+            this.btnRefresh.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnRefresh.ImageOptions.Image")));
+            this.btnRefresh.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnRefresh.ImageOptions.LargeImage")));
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnRefresh_ItemClick);
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1,
+            this.ribbonPageGroup2,
+            this.ribbonPageGroup3});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "Tác vụ";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnThem);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnCapnhat);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnXoa);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "Quản lý nội dung";
+            // 
+            // ribbonPageGroup2
+            // 
+            this.ribbonPageGroup2.ItemLinks.Add(this.btnDanhSachHoSo);
+            this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+            this.ribbonPageGroup2.Text = "Hồ sơ bệnh án";
+            // 
+            // ribbonPageGroup3
+            // 
+            this.ribbonPageGroup3.ItemLinks.Add(this.btnExit);
+            this.ribbonPageGroup3.ItemLinks.Add(this.btnRefresh);
+            this.ribbonPageGroup3.Name = "ribbonPageGroup3";
+            this.ribbonPageGroup3.Text = "Thoát";
+            // 
+            // cardView1
+            // 
+            this.cardView1.FocusedCardTopFieldIndex = 0;
+            this.cardView1.GridControl = this.gridControl1;
+            this.cardView1.Name = "cardView1";
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.gridControl1);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControl1.Location = new System.Drawing.Point(0, 143);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(758, 217);
+            this.groupControl1.TabIndex = 2;
+            this.groupControl1.Text = "Danh sách bệnh nhân";
+            // 
+            // DanhSachBenhNhan
+            // 
+            this.AllowFormGlass = DevExpress.Utils.DefaultBoolean.False;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 360);
+            this.Controls.Add(this.groupControl1);
+            this.Controls.Add(this.ribbonControl1);
+            this.Name = "DanhSachBenhNhan";
+            this.Ribbon = this.ribbonControl1;
+            this.Text = "Danh sách bệnh nhân";
+            this.Activated += new System.EventHandler(this.DanhSachBenhNhan_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DanhSachBenhNhan_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.benhNhansBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraBars.BarButtonItem btnThem;
+        private DevExpress.XtraBars.BarButtonItem btnCapnhat;
+        private DevExpress.XtraBars.BarButtonItem btnXoa;
+        private System.Windows.Forms.BindingSource benhNhansBindingSource;
+        private DevExpress.XtraGrid.Columns.GridColumn colMaYTe;
+        private DevExpress.XtraGrid.Columns.GridColumn colHoTen;
+        private DevExpress.XtraGrid.Columns.GridColumn colNamSinh;
+        private DevExpress.XtraGrid.Columns.GridColumn colGioiTinh;
+        private DevExpress.XtraGrid.Columns.GridColumn colDiaChi;
+        private DevExpress.XtraGrid.Columns.GridColumn colNgheNghiep;
+        private DevExpress.XtraGrid.Columns.GridColumn colSoDT;
+        private DevExpress.XtraGrid.Columns.GridColumn colLichSuKhamBenhs;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+        private DevExpress.XtraBars.BarButtonItem btnDanhSachHoSo;
+        private DevExpress.XtraGrid.Views.Card.CardView cardView1;
+        private DevExpress.XtraBars.BarButtonItem btnExit;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup3;
+        private DevExpress.XtraBars.BarButtonItem btnRefresh;
+    }
+}
+
---
+++ QuanLyBenhVien/QuanLyBenhVien/DanhSachBenhNhan.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using QTCC_QuanLyBenhVien.Model.DAO;
+using QTCC_QuanLyBenhVien.Model.DTO;
+
+namespace QTCC_QuanLyBenhVien
+{
+    public partial class DanhSachBenhNhan : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        private string _id;
+        private readonly BenhNhanDAO _bnDao = new BenhNhanDAO();
+        private readonly QLBenhVienDBContext _dbContext = new QLBenhVienDBContext();
+        private readonly LichSuKhamBenhDAO _lsDao = new LichSuKhamBenhDAO();
+        public DanhSachBenhNhan()
+        {
+            InitializeComponent();
+            RefreshGridControl();
+            //_dbContext.BenhNhans.Load();
+            //benhNhansBindingSource.DataSource = _dbContext.BenhNhans.Local.ToBindingList();
+        }
+        
+        //Refresh lại GridControl
+        public void RefreshGridControl()
+        {
+            int currentRow = gridView1.FocusedRowHandle;
+            var blist = new BindingList<BenhNhan>(_bnDao.GetAllBenhnhan());
+            var bsource = new BindingSource(blist, null);
+            gridControl1.DataSource = bsource;
+            gridView1.FocusedRowHandle = currentRow;
+            //gridControl1.RefreshDataSource();
+        }
+
+        private void gridView1_RowUpdated(object sender, DevExpress.XtraGrid.Views.Base.RowObjectEventArgs e)
+        {
+            _dbContext.SaveChanges();
+        }
+
+        private void btnThem_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form.GetType() == typeof(ThemCapNhapBenhNhan))
+                {
+                    form.WindowState = FormWindowState.Normal;
+                    form.Activate();
+                    return;
+                }
+            }
+            ThemCapNhapBenhNhan thembn = new ThemCapNhapBenhNhan();
+            thembn.Show();
+        }
+
+        private void gridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            try
+            {
+                _id = gridView1.GetRowCellValue(e.FocusedRowHandle, "MaYTe").ToString();
+            }
+            catch (Exception)
+            {
+                _id = null;
+            }
+        }
+
+        private void btnCapnhat_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form.GetType() == typeof(ThemCapNhapBenhNhan))
+                {
+                    form.WindowState = FormWindowState.Normal;
+                    form.Activate();
+                    return;
+                }
+            }
+            var bn = _bnDao.GetBenhNhanById(_id);
+            if (bn != null)
+            {
+                ThemCapNhapBenhNhan thembn = new ThemCapNhapBenhNhan(bn,1);
+                thembn.Show();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn 1 bệnh nhân để cập nhật");
+            }
+        }
+
+        private void btnDanhSachHoSo_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if (_lsDao.CountBenhAn(_id) > 0)
+            {
+                foreach (Form form in Application.OpenForms)
+                {
+                    if (form.GetType() == typeof (HoSoKhamBenh))
+                    {
+                        form.WindowState = FormWindowState.Normal;
+                        form.Activate();
+                        return;
+                    }
+                }
+                BenhNhan bn = _bnDao.GetBenhNhanById(_id);
+                HoSoKhamBenh hoso = new HoSoKhamBenh(bn);
+                hoso.Text = @"Bệnh nhân-" + bn.MaYTe + @"-" + bn.HoTen;
+                hoso.Show();
+            }
+            else
+            {
+                foreach (Form form in Application.OpenForms)
+                {
+                    if (form.GetType() == typeof(ThemCapNhatBenhAn))
+                    {
+                        form.WindowState = FormWindowState.Normal;
+                        form.Activate();
+                        return;
+                    }
+                }
+                ThemCapNhatBenhAn themba = new ThemCapNhatBenhAn(_id);
+                themba.Show();
+            }
+        }
+
+        private void btnXoa_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            var dialogResult = MessageBox.Show(@"Bạn có chắc chắn xóa dòng này?", @"Xác nhận",
+                    MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //do somethingif (_lsDao.CountBenhAn(_id) > 0)
+                {
+                    var dialogResult2 = MessageBox.Show(@"Bạn có chắc chắn xóa : bệnh án của bệnh nhân và thông tin bệnh nhân?", @"Xác nhận",
+                    MessageBoxButtons.YesNo);
+                    if (dialogResult2 == DialogResult.Yes)
+                    {
+                        var result = _bnDao.DeleteBenhNhan(_id);
+                        if (result)
+                        {
+                            MessageBox.Show(@"Xóa thành công!");
+                            RefreshGridControl();
+                        }
+                        else
+                        {
+                            MessageBox.Show(@"Xóa không thành công!");
+                        }
+                    }
+                }
+                
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
+        }
+
+        private void DanhSachBenhNhan_Activated(object sender, EventArgs e)
+        {
+            RefreshGridControl();
+        }
+
+        private void DanhSachBenhNhan_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var close = MessageBox.Show(@"Bạn có đồng ý thoát chương trình?", @"Thông báo", MessageBoxButtons.YesNo);
+            if (close != DialogResult.Yes)
+            {
+                //MessageBox.Show(@"The application has been closed successfully.", @"Application Closed!", MessageBoxButtons.OK);
+                e.Cancel = true;
+                Activate();
+            }
+            else
+            {
+                foreach (Form form in Application.OpenForms)
+                {
+                    if (form.GetType() == typeof(LoginForm))
+                    {
+                        form.WindowState = FormWindowState.Normal;
+                        //form.Activate();
+                        form.Close();
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void btnExit_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Close();
+        }
+        
+        private void btnRefresh_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            RefreshGridControl();
+        }
+
+        private void gridView1_RowCountChanged(object sender, EventArgs e)
+        {
+            RefreshGridControl();
+        }
+
+        private void gridControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
