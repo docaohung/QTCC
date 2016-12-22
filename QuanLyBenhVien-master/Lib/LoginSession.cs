d28e18ca37250cbc1c12d0121240669c76f2b63a
Do Cao Hung <conheo4@gmail.com>
2016-12-22 20:52:24 +0700

    del

--- QuanLyBenhVien-master/Lib/LoginSession.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace QTCC_QuanLyBenhVien.Lib
-{
-    public static class LoginSession
-    {
-        public static string Username { get; set; }
-        public static string Name { get; set; }
-    }
-}
